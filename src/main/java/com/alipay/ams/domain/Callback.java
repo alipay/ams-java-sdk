bd1b80a5a61bdf2905f3825a2960a894459efe41
guangling.zgl <zhanggl1981@gmail.com>
2019-11-04 21:30:03 +0800

    Fixed MissingFormatArgumentException

--- src/main/java/com/alipay/ams/domain/Callback.java
+++ src/main/java/com/alipay/ams/domain/Callback.java
@@ -49,7 +49,7 @@ public abstract class Callback<R extends Request, P extends Response> extends
     public void onSignatureVerifyFailed(R request, ResponseHeader responseHeader,
                                         String responseBody) {
         throw new IllegalStateException(String.format(
-            "SignatureVerifyFailed[%s]: %s[bizId=[%s]], responseBody=[%s]", request.getClass()
+            "SignatureVerifyFailed[%s]: [bizId=[%s]], responseBody=[%s]", request.getClass()
                 .getName(), request.getBizIdentifier(), responseBody));
     }
 
